b5678393138ae16575482770889debf8173c2ec0
dwcs-2324 <148275196+dwcs-2324@users.noreply.github.com>
2023-11-10 14:22:51 +0100

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,12 @@
 
 
 
-
+/**
+ * Summary of getConnection
+ * @param mixed $file fichero INI con la configuración de la conexión. Si no se envía se leerá por defecto db_settings.ini en el mismo directorio
+ * @throws \exception En caso de error leyendo el fichero
+ * @return PDO|null un objeto PDO si ha habido éxito creando la conexión, null en caso contrario
+ */
 function getConnection()
 {
 
